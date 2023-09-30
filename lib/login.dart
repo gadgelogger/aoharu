087baff4bf7c2a4d31a353d87b8252920f3ff3d9
Yuta Wakamatsu <wakka0014@gmail.com>
2023-10-01 05:17:11 +0900

    ログイン失敗時にスナックバーを表示

--- lib/login.dart
+++ lib/login.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:teamc/home_page.dart';
 import 'package:teamc/manage.dart';
+import 'package:teamc/show_snack_bar.dart';
 import 'package:teamc/signup.dart';
 
 class Login extends StatefulWidget {
@@ -120,6 +120,7 @@ class _LoginState extends State<Login> {
                       setState(() {
                         infoText = "ログインNG：${e.toString()}";
                       });
+                      showSnackBar(context, "メールアドレスまたはパスワードが違います。");
                     }
                   } else {
                     setState(() {
