4effc8fb2b9fb2d20ae81d7cc7a683df60e16504
gadgelogger <sarayou273142910@gmail.com>
2023-09-30 16:07:53 +0900

    login.dartとsigin.dartを追加しました。

---
+++ lib/signin.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Signin extends StatefulWidget {
+  const Signin({Key? key}) : super(key: key);
+
+  @override
+  State<Signin> createState() => _SigninState();
+}
+
+class _SigninState extends State<Signin> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
