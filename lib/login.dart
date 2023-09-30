6cbbf8c02181dbbca9d7462169f022a4190eb473
gadgelogger <sarayou273142910@gmail.com>
2023-09-30 18:22:01 +0900

    ログイン・サインイン画面でキーボードのフォーカスが正常に動作するように修正

--- lib/login.dart
+++ lib/login.dart
@@ -15,90 +15,95 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Container(
-            alignment: Alignment.topLeft,
-            child: const Stack(
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.only(
-                    top: 110,
-                    left: 20,
+        body: GestureDetector(
+      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+      behavior: HitTestBehavior.opaque,
+      child: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              alignment: Alignment.topLeft,
+              child: const Stack(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(
+                      top: 110,
+                      left: 20,
+                    ),
+                    child: Text('Welcome',
+                        style: TextStyle(
+                            fontSize: 60.0, fontWeight: FontWeight.bold)),
                   ),
-                  child: Text('Welcome',
-                      style: TextStyle(
-                          fontSize: 60.0, fontWeight: FontWeight.bold)),
+                ],
+              ),
+            ),
+            const SizedBox(height: 200),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: TextField(
+                controller: _emailController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'メールアドレス',
                 ),
-              ],
-            ),
-          ),
-          const SizedBox(height: 200),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: TextField(
-              controller: _emailController,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'メールアドレス',
               ),
             ),
-          ),
-          const SizedBox(height: 20),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: TextField(
-              controller: _passwordController,
-              obscureText: true,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'パスワード',
-              ),
-            ),
-          ),
-          const SizedBox(height: 20),
-          SizedBox(
-            height: 50,
-            width: 300,
-            child: ElevatedButton(
-              onPressed: () {
-                // TODO: Implement login functionality
-              },
-              child: const Text('ログイン'),
-              style: ElevatedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30),
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: TextField(
+                controller: _passwordController,
+                obscureText: true,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'パスワード',
                 ),
-                elevation: 0,
               ),
             ),
-          ),
-          const SizedBox(height: 20),
-          SizedBox(
-            height: 50,
-            width: 300,
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const Signin()),
-                );
-              },
-              child: const Text(
-                '新規会員登録はこちら',
-                style: TextStyle(color: Colors.black),
-              ),
-              style: ElevatedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30),
+            const SizedBox(height: 20),
+            SizedBox(
+              height: 50,
+              width: 300,
+              child: ElevatedButton(
+                onPressed: () {
+                  // TODO: Implement login functionality
+                },
+                child: const Text('ログイン'),
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  elevation: 0,
                 ),
-                backgroundColor: Colors.grey[300],
-                elevation: 0,
               ),
             ),
-          ),
-        ],
+            const SizedBox(height: 20),
+            SizedBox(
+              height: 50,
+              width: 300,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const Signin()),
+                  );
+                },
+                child: const Text(
+                  '新規会員登録はこちら',
+                  style: TextStyle(color: Colors.black),
+                ),
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  backgroundColor: Colors.grey[300],
+                  elevation: 0,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
-    );
+    ));
   }
 }
